2160c749b071692dca282b5b4553595a8c077d46
Ansatasia Gorodinskaya <agorodinskaya@gmail.com>
2018-06-06 09:48:58 +1000

    happy days

--- airlines-server/app/controllers/session_controller.rb
+++ airlines-server/app/controllers/session_controller.rb
@@ -3,8 +3,22 @@ class SessionController < ApplicationController
   end
 
   def create
+
+    user = User.find_by email: params[:email]
+    if user.present? && user.authenticate( params[:password] )
+
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else
+
+      flash[:error] = "Wrong e-mail or password"
+      redirect_to login_path
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
--- airlines-server/config/routes.rb
+++ airlines-server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  get 'pages/new'
+  root to: "pages#home"
   get "/airplanes" => "airplanes#index"
 
   get "/login" => "session#new"
