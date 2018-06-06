ce95c8a8a544fc64ad0a27aece8e76f76c1ab96b
Ansatasia Gorodinskaya <agorodinskaya@gmail.com>
2018-06-06 11:40:58 +1000

    homepage draft

--- airlines-server/app/controllers/pages_controller.rb
+++ airlines-server/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     api_key_news = Rails.application.secrets.news_api_key
-    url_news = "https://newsapi.org/v2/everything?q=(flights)&apiKey=#{api_key_news}"
+    url_news = "https://newsapi.org/v2/everything?q=(weather)&apiKey=#{api_key_news}"
     results_news = HTTParty.get( url_news )
     @results_news = results_news
     # raise "hell"
