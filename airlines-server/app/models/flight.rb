18b7f3cca4c6bbcf94a6b9f3586c1b2088a3a6c9
Scott Lee <achacttn@gmail.com>
2018-06-06 10:55:56 +1000

    nothing is working

--- airlines-server/app/models/flight.rb
+++ airlines-server/app/models/flight.rb
@@ -1,7 +1,7 @@
 class Flight < ApplicationRecord
     belongs_to :airplane
-    has_many :users, through: :reservations
     has_many :reservations
+    has_many :users, through: :reservations
     # validates :title, :body, :presence => true
     # validates :title, :length => { :minimum => 2 }
     # validates :title, uniqueness: true
