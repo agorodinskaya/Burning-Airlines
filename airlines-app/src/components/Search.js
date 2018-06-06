2a7c73d35e58e9af9478f787d63dcd5d35053572
Scott Lee <achacttn@gmail.com>
2018-06-06 16:13:26 +1000

    component beginning

---
+++ airlines-app/src/components/Search.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+class Search extends Component {
+    constructor(){
+        super();
+
+        
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>HELLO</h1>
+            </div>
+        )
+    }
+}
+
+export default Search;
