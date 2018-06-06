3d0c3eb22e8c5e563b85de4853ed12e07a86b391
Scott Lee <achacttn@gmail.com>
2018-06-06 10:26:34 +1000

    seed name change

--- airlines-server/db/seeds.rb
+++ airlines-server/db/seeds.rb
@@ -18,14 +18,14 @@ air4 = Airplane.create airplane_name: "777-VZ", airline_company: 'Qantas', row: 
 
 # Flight.destroy_all
 
-# f1 = Flight.create airplane_name: "747-NE", reservation_id: , datetime: "2010-10-10 08:30:30", origin: "Sydney", destination: "Perth"
-# f2 = Flight.create airplane_name: "747-NE", reservation_id: , datetime: "2010-10-11 22:15:15", origin: "Perth", destination: "Sydney"
-# f3 = Flight.create airplane_name: "777-VU", reservation_id: , datetime: "2010-05-06 11:20:10", origin: "Sydney", destination: "Melbourne"
-# f4 = Flight.create airplane_name: "777-VU", reservation_id: , datetime: "2010-05-07 15:45:05", origin: "Melbourne", destination: "Sydney"
-# f5 = Flight.create airplane_name: "747-NQ", reservation_id: , datetime: "2010-10-12 02:00:25", origin: "Melbourne", destination: "Auckland"
-# f6 = Flight.create airplane_name: "747-NQ", reservation_id: , datetime: "2010-10-13 07:35:20", origin: "Auckland", destination: "Melbourne"
-# f7 = Flight.create airplane_name: "777-VZ", reservation_id: , datetime: "2011-01-03 18:51:20", origin: "Auckland", destination: "Sydney"
-# f8 = Flight.create airplane_name: "777-VZ", reservation_id: , datetime: "2011-01-04 16:21:40", origin: "Sydney", destination: "Auckland"
+# f1 = Flight.create airplane_name: "747-NE", reservation_id: , datetime: "2010-10-10 08:30:30", origin: "SYD", destination: "PER"
+# f2 = Flight.create airplane_name: "747-NE", reservation_id: , datetime: "2010-10-11 22:15:15", origin: "PER", destination: "SYD"
+# f3 = Flight.create airplane_name: "777-VU", reservation_id: , datetime: "2010-05-06 11:20:10", origin: "SYD", destination: "MEL"
+# f4 = Flight.create airplane_name: "777-VU", reservation_id: , datetime: "2010-05-07 15:45:05", origin: "MEL", destination: "SYD"
+# f5 = Flight.create airplane_name: "747-NQ", reservation_id: , datetime: "2010-10-12 02:00:25", origin: "MEL", destination: "AKL"
+# f6 = Flight.create airplane_name: "747-NQ", reservation_id: , datetime: "2010-10-13 07:35:20", origin: "AKL", destination: "MEL"
+# f7 = Flight.create airplane_name: "777-VZ", reservation_id: , datetime: "2011-01-03 18:51:20", origin: "AKL", destination: "SYD"
+# f8 = Flight.create airplane_name: "777-VZ", reservation_id: , datetime: "2011-01-04 16:21:40", origin: "SYD", destination: "AKL"
 
 # air1 << f1 << f2
 # air2 << f3 << f4
@@ -36,11 +36,11 @@ air4 = Airplane.create airplane_name: "777-VZ", airline_company: 'Qantas', row: 
 puts "Created #{ Airplane.all.length } airplanes."
 
 Flight.destroy_all
-f1 = Flight.create date: "2010-10-10 08:30:30", origin: "Sydney", destination: "Perth"
-f2 = Flight.create date: "2010-05-06 11:20:10", origin: "Sydney", destination: "Melbourne"
-f3 = Flight.create date: "2010-10-12 02:00:25", origin: "Melbourne", destination: "Auckland"
-f4 = Flight.create date: "2011-01-03 18:51:20", origin: "Auckland", destination: "Sydney"
-f5 = Flight.create date: "2010-12-12 08:30:30", origin: "Sydney", destination: "Perth"
+f1 = Flight.create date: "2010-10-10 08:30:30", origin: "SYD", destination: "PER"
+f2 = Flight.create date: "2010-05-06 11:20:10", origin: "SYD", destination: "MEL"
+f3 = Flight.create date: "2010-10-12 02:00:25", origin: "MEL", destination: "AKL"
+f4 = Flight.create date: "2011-01-03 18:51:20", origin: "AKL", destination: "SYD"
+f5 = Flight.create date: "2010-12-12 08:30:30", origin: "SYD", destination: "PER"
 
 air1.flights << f1 << f5
 air2.flights << f2
